7ccba33794a6d53e8c7bc6741270a8a2706d7b81
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2026-01-26 15:42:24 +0200

    Fix application

--- src/main/java/com/github/vbychkovskyi/uptimebot/UptimebotApplication.java
+++ src/main/java/com/github/vbychkovskyi/uptimebot/UptimebotApplication.java
@@ -1,7 +1,9 @@
 package com.github.vbychkovskyi.uptimebot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UptimebotApplication {
 
   static void main(String[] args) {

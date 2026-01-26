4cef34fe9522189ff04dd638eed4af9c8880d106
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2026-01-26 17:47:54 +0200

    Implement monitor creation with repository and database sequence integration
    - Added `createMonitor` method in `MonitorServiceImpl`.
    - Updated `MonitorEntity` with ID generation using a database sequence.
    - Modified Liquibase changelog to include sequence creation for monitors.

--- src/main/java/com/github/vbychkovskyi/uptimebot/orm/entity/MonitorEntity.java
+++ src/main/java/com/github/vbychkovskyi/uptimebot/orm/entity/MonitorEntity.java
@@ -5,7 +5,10 @@ import java.time.Duration;
 import io.hypersistence.utils.hibernate.type.interval.PostgreSQLIntervalType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +21,12 @@ import org.hibernate.annotations.Type;
 public class MonitorEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "monitor_id_sequence")
+  @SequenceGenerator(
+    name = "monitor_id_sequence",
+    sequenceName = "monitor_id_sequence",
+    allocationSize = 1
+  )
   private Long id;
 
   private String name;
--- src/main/java/com/github/vbychkovskyi/uptimebot/service/impl/MonitorServiceImpl.java
+++ src/main/java/com/github/vbychkovskyi/uptimebot/service/impl/MonitorServiceImpl.java
@@ -2,14 +2,27 @@ package com.github.vbychkovskyi.uptimebot.service.impl;
 
 import com.github.vbychkovskyi.uptimebot.api.model.InputMonitor;
 import com.github.vbychkovskyi.uptimebot.api.model.Monitor;
+import com.github.vbychkovskyi.uptimebot.orm.entity.MonitorEntity;
+import com.github.vbychkovskyi.uptimebot.orm.entity.MonitorRepository;
 import com.github.vbychkovskyi.uptimebot.service.MonitorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MonitorServiceImpl implements MonitorService {
 
+  private final MonitorRepository monitorRepository;
+
   @Override
+  @Transactional
   public Monitor createMonitor(final InputMonitor monitor) {
-    return null;
+    final var monitorEntity = new MonitorEntity();
+    monitorEntity.setName(monitor.name());
+    monitorEntity.setSchedule(monitor.schedule());
+    monitorEntity.setUrl(monitor.url());
+    final var save = monitorRepository.save(monitorEntity);
+    return new Monitor(save.getId(), monitor.name(), monitor.url(), monitor.schedule());
   }
 }
